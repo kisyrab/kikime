82353cff2ee9eb0ed9edca19d55c9bc4b9baefd1
kisyrab <906ksy@naver.com>
2024-08-05 17:51:45 +0900

    commit3

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".img_box2").slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        slidesToShow:4,
+        slidesToScroll:4,
+        arrows:false,
+        initialSlide: 1,
+        centerPadding: '70px', 
+    });
+})//j
