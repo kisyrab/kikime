058bcb7e36683ae1b3614110280a7beb91b7c531
kisyrab <906ksy@naver.com>
2024-08-08 17:43:03 +0900

    8/8 commit

--- js/main.js
+++ js/main.js
@@ -8,4 +8,45 @@ $(function(){
         initialSlide: 1,
         centerPadding: '70px', 
     });
+
+    //탭메뉴
+    // $(".tab li a").click(function(){
+    //     $(".con1 h2").removeClass("on");
+    //     $(this).addClass("on");
+    
+    //     $(".con1 ul").hide();
+    //     $(this).next().css({display : "flex"})
+    
+    // })
+
+
+
+
+    bg = $(".tab li").eq(0).attr("bg")
+    $(".tab li").eq(0).css({"background" : bg })
+
+
+    $(".tab li").click(function(){
+        bg = $(this).attr("bg")
+        $(this).siblings().find("a").addClass("on");
+        $(this).find("a").removeClass("on");
+        var i = $(this).index()
+        console.log(i)
+        $(".frame .section").hide();
+        // $(".frame .section").eq(i).css({display : "flex"})
+        $(".frame .section").eq(i).fadeIn();
+        $(".tab li").eq(i).css({"background" : bg })
+        $(".tab li").eq(i).siblings().css({"background" : "transparent" })
+    })
+
+
+    $(".tab li").hover(function(){
+        var bg = $(this).attr("bg");
+        $(this).css({"background" : bg })
+    }, function(){
+        $(this).siblings().css({"background" : "transparent" })
+        
+    })//hover
+
+
 })//j
